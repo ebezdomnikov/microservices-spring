199baac3611219314aea900dc35f4125a4363110
Evgenii Bezdomnikov <evgenii.bezdomnikov@hemmersbach.com>
2019-07-13 13:40:21 +0200

    First commit

---
+++ dispatcher-service-api/src/test/java/com/ac/microservices/DispatcherServiceApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ac.microservices;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DispatcherServiceApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
---
+++ registry-service/src/main/java/com/ac/microservices/ServiceApplication.java
@@ -0,0 +1,18 @@
+package com.ac.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEurekaServer
+@SpringBootApplication
+public class ServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApplication.class, args);
+    }
+
+}

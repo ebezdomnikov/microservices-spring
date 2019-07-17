ef3955faa5f0dc48e77948bd7fb6107325b1096e
Evgeniy Bezdomnikov <e.bezdomnikov@gmail.com>
2019-07-17 17:34:36 +0200

    Update CreateProductEvent.java

--- dispatcher-service-api/src/main/java/com/ac/microservices/model/CreateProductEvent.java
+++ dispatcher-service-api/src/main/java/com/ac/microservices/model/CreateProductEvent.java
@@ -63,29 +63,3 @@ public class CreateProductEvent extends Event {
     }
 }
 
-//{
-//        "id": "CG7088",
-//        "name": "Nite Jogger Shoes",
-//        "model_number": "BTO93",
-//        "product_type": "inline",
-//        "meta_data": {
-//        "page_title": "adidas Nite Jogger Shoes - Black | adidas UK",
-//        "site_name": "adidas United Kingdom",
-//        "description": "Shop for Nite Jogger Shoes - Black at adidas.co.uk! See all the styles
-//        and colours of Nite Jogger Shoes - Black at the official adidas UK online store.",
-//        "keywords": "Nite Jogger Shoes",
-//        "canonical": "//www.adidas.co.uk/nite-jogger-shoes/CG7088.html"
-//        },
-//        "pricing_information": {
-//        "standard_price": 119.95,
-//        "standard_price_no_vat": 99.96,
-//        "currentPrice": 119.95
-//        },
-//        "product_description": {
-//        "title": "Nite Jogger Shoes",
-//        "subtitle": "Modern cushioning updates this flashy '80s standout.",
-//        "text": "Inspired by the 1980 Nite Jogger, these shoes shine bright with retro style and
-//        reflective details. The mesh and nylon ripstop upper is detailed with suede overlays. An
-//        updated Boost midsole adds responsive cushioning for all-day comfort."
-//        }
-//        }
